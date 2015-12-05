b0e42d172ae8b392e2dcdd2f95ab3d5bc212bbc8
Yoshihide Jimbo <yjimbo@gmail.com>
2015-12-06 00:33:17 +0900

    Add link to repository on github

--- app/components/app/app.ts
+++ app/components/app/app.ts
@@ -1,13 +1,15 @@
 import {Component} from 'angular2/angular2';
 import {AppTitleComponent} from '../app-title/app-title';
 import {ProfileCardsListComponent} from '../profile-cards-list/profile-cards-list';
+import {Octocat} from '../octocat/octocat';
 
 @Component({
   selector: 'my-app',
   template: `
     <app-title></app-title>
     <profile-cards-list></profile-cards-list>
+    <octocat></octocat>
   `,
-  directives: [AppTitleComponent, ProfileCardsListComponent]
+  directives: [AppTitleComponent, ProfileCardsListComponent, Octocat]
 })
 export class AppComponent {}
---
+++ app/components/octocat/octocat.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+  selector: 'octocat',
+  templateUrl: './components/octocat/octocat.html'
+})
+export class Octocat {}
